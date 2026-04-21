d35119e339225a65841e9f8a37d4117168255d9e
Marcos <marcoskong2@gmail.com>
2026-04-21 11:45:43 +0200

    Añadido de botones al panel

--- resources/views/cliente/account.blade.php
+++ resources/views/cliente/account.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Mi Panel - Cliente')
 @section('content')
 <div class="container mt-5">
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-12">
             <h2 class="mb-4">Hola, {{ Auth::user()->nombre }}</h2>
             <div class="alert alert-info">
@@ -23,12 +23,35 @@
                 <button class="btn btn-outline-primary">Ver reservas</button>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card shadow-sm text-center p-3">
+                <h4>Notificaciones</h4>
+                <p>Mira tus notificaciones.</p>
+                <button class="btn btn-outline-primary">Ver notificaciones</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="card shadow-sm text-center p-3">
+                <h4>Foros</h4>
+                <p>Navega en los foros con otros usuarios.</p>
+                <button class="btn btn-outline-primary">Ver foros</button>
+            </div>
         </div>
         <div class="col-md-4">
             <div class="card shadow-sm text-center p-3">
                 <h4>Productos favoritos</h4>
                 <p>Échale un ojo a tus productos favoritos.</p>
                 <button class="btn btn-outline-primary">Ver favoritos</button>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card shadow-sm text-center p-3">
+                <h4>Mi cuenta</h4>
+                <p>Edita tu cuenta.</p>
+                <button class="btn btn-outline-primary">Ver cuenta</button>
             </div>
         </div>
     </div>
